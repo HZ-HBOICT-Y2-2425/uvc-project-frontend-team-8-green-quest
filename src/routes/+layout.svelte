89badde3fdf25e8807f71496419e62a0cb41176c
Polly.Vl <vlad0004@hz.nl>
2024-11-19 11:02:41 +0100

    header, footer and colors are added (colors are in the tailwind.config.js file)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,40 @@
 <script>
-	import "../app.css";
+    import "../app.css";
 </script>
 
-<div>
+<!-- <div>
 	<header>
 		<p>Coins</p>
-        <!-- coins image -->
+         coins image 
         <p>CO2 saved</p>
         <p>user</p>
 	</header>
-	<main>
-		<slot />
-	</main>
+	<main> -->
+<div class="flex flex-col justify-between">
+    <header class="bg-green flex justify-between p-3 w-full h-full">
+        <div class="flex flex-row">
+            <img src="/coins.png" alt="coins" class="w-12 h-fit" />
+            <h2 class="text-2xl mt-1">2</h2>
+        </div>
+        <h2 class="text-3xl mt-2">CO2: 55</h2>
+        <button id="profile">
+            <img src="/profile_icon.png" alt="profile" class="w-12 h-fit" />
+        </button>
+    </header>
 
-	<footer>
-		<p>Shop</p>
-        <p>Leaderboard</p>
-        <p>Information</p>
-	</footer>
+    <main>
+        <slot />
+    </main>
+
+    <footer class="bg-green flex justify-between p-3 w-full h-full">
+        <button id="shop">
+            <img src="/shop.png" alt="shop" class="w-12 h-fit" />
+        </button>
+        <button id="leaderboard">
+            <h2 class="text-3xl p-2 bg-orange-red rounded-3xl">Leaderboard</h2>
+        </button>
+        <button id="info">
+            <img src="/info.png" alt="information" class="w-10 h-fit mt-1" />
+        </button>
+    </footer>
 </div>
