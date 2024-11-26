840c8659b8be487614bae3d2a9656d6a356b6c99
Andrea Bezzolato <bezz0001@hz.nl>
2024-11-26 12:49:43 +0100

    Run dev fix

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	it: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/uvc-project-frontend-team-8-green-quest' : '', // Use your repo name here
-		},
-	}
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
