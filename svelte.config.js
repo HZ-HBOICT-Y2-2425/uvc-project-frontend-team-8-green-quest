f160bdb7818a6cbc5579ac92ab8001abf3204d96
Andrea Bezzolato <bezz0001@hz.nl>
2024-11-26 14:20:04 +0100

    Final test

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'inde.html',
-      precompress: false,
-      strict: false
-    }),
-    paths: {
-      base: '/uvc-project-frontend-team-8-green-quest',
-    },
-    prerender: {
-      entries: [
-        '/',          // Pre-render the home page
-        '/challenges', 
-		'/challenges/1',
-		'/challenges/2',
-		'/challenges/3',    // Example: Pre-render specific static routes
-        '/shop',    // Example: Pre-render dynamic routes
-        ],
-    },
-  },
-  preprocess: vitePreprocess(),
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
