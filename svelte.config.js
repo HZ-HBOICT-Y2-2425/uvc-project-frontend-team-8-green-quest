ba59666e170c6cb2ebde7d529debd2fe4f4e43d7
Andrea Bezzolato <bezz0001@hz.nl>
2024-11-26 13:33:30 +0100

    Dynamic routes added

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,19 @@ const config = {
       pages: 'build',
       assets: 'build',
       fallback: null,
+      precompress: false,
+      strict: false
     }),
     paths: {
       base: process.env.NODE_ENV === 'production' ? '/uvc-project-frontend-team-8-green-quest' : '',
-    }
+    },
+    prerender: {
+      entries: [
+        '/',          // Pre-render the home page
+        '/challenges',     // Example: Pre-render specific static routes
+        '/shop',    // Example: Pre-render dynamic routes
+        ],
+    },
   },
   preprocess: vitePreprocess(),
 };
