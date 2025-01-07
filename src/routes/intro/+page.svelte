0e80362f8871ff6593f0854738757ed9c6abb6bd
Polly.Vl <vlad0004@hz.nl>
2025-01-07 10:55:40 +0200

    removed the lauout from intro page

--- src/routes/intro/+page.svelte
+++ src/routes/intro/+page.svelte
@@ -1,25 +1,3 @@
-<!-- <section class="h-screen">
-    <h1 class="m-2 text-3xl text-center font-bold">Welcome to Green Quest</h1>
-    <h2 class="m-2 text-2xl font-semibold">
-        Change Habits, Reduce CO2, Build a Greener World
-    </h2>
-    <p class="m-4 text-lg">
-        Green Quest is a habit-changing app that encourages young adults to
-        adopt sustainable behaviors. Using gamification, personalized
-        challenges, and progress tracking, you can take small steps that make a
-        big difference for the planet.
-    </p>
-    <p class="m-4 text-lg">
-        Start your journey with an empty garden. Complete daily challenges to
-        earn coins, grow your garden, and track the CO2 you save. Together, we
-        can build a sustainable future.
-    </p>
-    <div class="m-6 space-x-4 flex flex-row justify-center">
-        <a href="/register" class="p-4 bg-green rounded-full"> Register </a>
-        <a href="/login" class="p-4 bg-green rounded-full"> Login </a>
-    </div>
-</section> -->
-
 <section class="bg-gradient-to-br from-green-100 via-blue-100 to-green-200 text-gray-800">
     <div class="p-6 text-center">
         <h1 class="text-4xl font-bold">🌿 Welcome to Green Quest 🌍</h1>
